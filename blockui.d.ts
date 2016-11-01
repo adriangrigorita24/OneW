c860178292e9ef34fed8ea8cb8327f8c89a07434
Lucia Plugaru <lucia.plugaru@aaltoinvest.com>
2016-11-01 15:39:41 +0200

    updated blockui definition files with the rest of the methods

--- blockui.d.ts
+++ blockui.d.ts
@@ -1,5 +1,9 @@
 interface BlockUI {
     init(): void;
+    block(el: HTMLElement, size: number): void;
+    unblock(el: HTMLElement): void;
+    getElement(selector: string): HTMLElement|HTMLElement[];
+    findElement(selector: string): HTMLElement|HTMLElement[];
 }
 
 declare var BlockUI: BlockUI;
