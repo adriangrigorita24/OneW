ecefc6cef01bc025d1d9ce30fa3c95d4fe0063bd
Lucia Plugaru <lucia.plugaru@aaltoinvest.com>
2016-12-16 17:11:20 +0200

    updated the typings for mdb

--- mdb.d.ts
+++ mdb.d.ts
@@ -1,8 +1,32 @@
 interface OnewMDB {
     init(): void;
-    datePicker(): void;
-    materialSelect():void;
-    countCharacters():void;
+
+    datePicker(options?: any): void;
+    timePicker(options?: any): void;
+
+    modalInit(options?: any):void;
+    modalAction(action):void;
+
+    tooltipInit(options?: any):void;
+    tooltipAction(action, selector?: string):void;
+
+    selectInit(selector?: string):void;
+    selectDestroy(selector?: string):void;
+
+    sideNavInit(options?: any):void;
+    sideNavAction(action?: any):void;
+
+    materialChipInit(data, selector?: string):void;
+    materialChipAction(action):void;
+    materialChipPlaceholder(placeholder, secondaryPlaceholder):void;
+
+    characterCounter():void;
+
+    dropdownInit(action?: string):void;
+
+    tabsInit(action):void;
+
+    lightbox():void;
 }
 
 declare let OnewMDB: OnewMDB;
